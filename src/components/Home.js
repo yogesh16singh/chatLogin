d450f0273a95a3d1ef148b35c06d860abaf7e39e
yogesh16singh <ys1997642@gmail.com>
2023-03-01 23:28:11 +0530

    updated Login

--- src/components/Home.js
+++ src/components/Home.js
@@ -18,41 +18,48 @@ function Home() {
         console.log(mob);
         console.log(mob.slice(7, 10))
 
+        // var digits = '0123456789';
+        // let OTP = '';
+        // for (let i = 0; i < 5; i++) {
+        //     OTP += digits[Math.floor(Math.random() * 10)];
+        // }
+        // console.log(OTP);
 
-        const encodedParams = new URLSearchParams();
-        encodedParams.append("txn_id", "17c6fa41-778f-49c1-a80a-cfaf7fae2fb8");
-        encodedParams.append("consent", "Y");
-        encodedParams.append("uidnumber", adh);
-        encodedParams.append("clientid", "222");
-        encodedParams.append("method", "uidvalidatev2");
 
-        const options = {
-            method: 'POST',
-            headers: {
-                'content-type': 'application/x-www-form-urlencoded',
-                'X-RapidAPI-Key': 'a1bcdf2ca7msh3d6d7f7704dc9e2p1aaeb7jsn64444f4d05ff',
-                'X-RapidAPI-Host': 'verifyaadhaarnumber.p.rapidapi.com'
-            },
-            body: encodedParams
-        };
+        // const encodedParams = new URLSearchParams();
+        // encodedParams.append("txn_id", "17c6fa41-778f-49c1-a80a-cfaf7fae2fb8");
+        // encodedParams.append("consent", "Y");
+        // encodedParams.append("uidnumber", adh);
+        // encodedParams.append("clientid", "222");
+        // encodedParams.append("method", "uidvalidatev2");
 
-        fetch('https://verifyaadhaarnumber.p.rapidapi.com/Uidverifywebsvcv1/VerifyAadhaarNumber', options)
-            .then(response => response.json())
-            .then(response => {
-                console.log(response);
-                console.log(response.Succeeded.Uid_Details.Data.verified);
-                console.log(response.Succeeded.Uid_Details.Data.mobile_number);
-                console.log(typeof response.Succeeded.Uid_Details.Data.mobile_number);
-                console.log(response.Succeeded.Uid_Details.Data.mobile_number.slice(7, 10));
-                if (response.Succeeded.Uid_Details.Data.mobile_number.slice(7, 10) != mob.slice(7, 10)) {
-                    swal("Error!", "Please enter correct details", "error");
-                }
-                else {
-                    console.log("details are correct");
-                }
+        // const options = {
+        //     method: 'POST',
+        //     headers: {
+        //         'content-type': 'application/x-www-form-urlencoded',
+        //         'X-RapidAPI-Key': 'a1bcdf2ca7msh3d6d7f7704dc9e2p1aaeb7jsn64444f4d05ff',
+        //         'X-RapidAPI-Host': 'verifyaadhaarnumber.p.rapidapi.com'
+        //     },
+        //     body: encodedParams
+        // };
 
-            })
-            .catch(err => console.error(err));
+        // fetch('https://verifyaadhaarnumber.p.rapidapi.com/Uidverifywebsvcv1/VerifyAadhaarNumber', options)
+        //     .then(response => response.json())
+        //     .then(response => {
+        //         console.log(response);
+        //         console.log(response.Succeeded.Uid_Details.Data.verified);
+        //         console.log(response.Succeeded.Uid_Details.Data.mobile_number);
+        //         console.log(typeof response.Succeeded.Uid_Details.Data.mobile_number);
+        //         console.log(response.Succeeded.Uid_Details.Data.mobile_number.slice(7, 10));
+        //         if (response.Succeeded.Uid_Details.Data.mobile_number.slice(7, 10) != mob.slice(7, 10)) {
+        //             swal("Error!", "Please enter correct details", "error");
+        //         }
+        //         else {
+        //             console.log("details are correct");
+        //         }
+
+        //     })
+        //     .catch(err => console.error(err));
 
 
 
